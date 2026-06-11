b7ec949a46a683a7e6f00e8e5d8c57c51d9087f4
Lcbx <luc.courbariaux@gmail.com>
2026-06-11 17:13:01 +0200

    rework the test launcher for convenience

--- main.py
+++ main.py
@@ -1,50 +1,48 @@
 #!python
 
-import argparse
-import sys
+from argparse import ArgumentParser
 from pathlib import Path as path
 from importlib import import_module
 
-parser = argparse.ArgumentParser(
+parser = ArgumentParser(
 	prog='python game engine',
 	description='WIP python game engine with custom ecs',
 	epilog='link: https://github.com/Lcbx/numpyEcs')
-parser.add_argument('scene', help='set scene script (to run or compile)')
-parser.add_argument('-t', '--tests', action='store_true', help='launches unit tests')
-parser.add_argument('--test', help='launches specific test')
+parser.add_argument('scene', nargs='?', help='set scene script (to run or compile)')
+parser.add_argument('-t', '--test', nargs='?', const=True, default=False, help='launches unit tests or a single test if specified by name')
 parser.add_argument('-c', '--compile', action='store_true', help='compile scene into standalone executable')
 args = parser.parse_args()
 
 normalize_path = lambda p: path(p).as_posix()
 to_module = lambda p: normalize_path(p).replace('/', '.').replace('.py', '')
 
-if args.test or args.tests:
-	from glob import glob
-	test_paths = glob('./tests/*.py')
+if not (args.scene or args.test):
+    parser.error("scene is required unless --test is used")
 
-# run particular test
-if args.test:
-	modules = [ import_module( to_module(test_path) ) for test_path in test_paths ]
-	failed = True
-	for m in modules:
-		try:
-			testFunc = getattr(m, args.test.split('::')[-1])
-			failed = False
-			testFunc()
-			print(f'{args.test} executed.')
-		except AttributeError as ex:
-			if not str(ex).startswith('module'):
-				raise ex
-	if failed:
-		print(f'could not find "{args.test}" test. candidates:')
-		from pprint import pp 
-		pp( list(f for m in modules for f in m.__dict__ )) # if 'test' in f) )
-
-# run whole test suite using pytest
-elif args.tests:
+# run test suite using pytest
+elif args.test:
 	import pytest
-	ret = pytest.main(['-q'] + test_paths )
-	sys.exit(ret)
+	from glob import glob
+	from sys import exit
+	test_paths = glob('./tests/*.py')
+	pytest_args = test_paths
+
+	# run particular test
+	if type(args.test) is str and (test_name := args.test).startswith('test'):
+		module_path = [ tp for tp in test_paths
+			if hasattr((mod := import_module( to_module(tp) )), test_name)
+		]
+
+		if not module_path:
+			print(f'could not find "{args.test}" test. candidates:')
+			from pprint import pp 
+			pp( list(f for m in modules for f in m.__dict__ ))
+			exit(1)
+
+		pytest_args = [ f'{module_path[0]}::{test_name}']
+	ret = pytest.main(['-qs', *pytest_args] )
+	exit(ret)
+		
 
 # run or compile the scene
 else:
