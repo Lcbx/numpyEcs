7bf011107f32a459a86e3f3e24c0f3776cab54ab
Lcbx <luc.courbariaux@gmail.com>
2026-09-05 21:24:54 +0200

    reorganize dir structure, minor main.py improvements

--- main.py
+++ main.py
@@ -1,58 +1,60 @@
 #!python
 
-from argparse import ArgumentParser
-from pathlib import Path as path
-from importlib import import_module
+if __name__ == "__main__":
+	import sys
+	from argparse import ArgumentParser
+	from pathlib import Path
+	from importlib import import_module
 
-parser = ArgumentParser(
-	prog='python game engine',
-	description='WIP python game engine with custom ecs',
-	epilog='link: https://github.com/Lcbx/numpyEcs')
-parser.add_argument('scene', nargs='?', help='set scene script (to run or compile)')
-parser.add_argument('-t', '--test', nargs='?', const=True, default=False, help='launches unit tests or a single test if specified by name')
-parser.add_argument('-c', '--compile', action='store_true', help='compile scene into standalone executable')
-args = parser.parse_args()
+	import src
 
-normalize_path = lambda p: path(p).as_posix()
-to_module = lambda p: normalize_path(p).replace('/', '.').replace('.py', '')
+	parser = ArgumentParser(
+		prog='python game engine',
+		description='WIP python game engine with custom ecs',
+		epilog='link: https://github.com/Lcbx/numpyEcs')
+	parser.add_argument('scene', nargs='?', help='set scene script (to run or compile)')
+	parser.add_argument('-t', '--test', nargs='?', const=True, default=False, help='launches unit tests or a single test if specified by name')
+	parser.add_argument('-c', '--compile', action='store_true', help='compile scene into standalone executable')
+	args = parser.parse_args()
 
-if not (args.scene or args.test or args.compile):
-    parser.error("scene is required unless --test or --compile are used")
+	normalize_path = lambda p: Path(p).as_posix()
+	to_module = lambda p: normalize_path(p).replace('/', '.').replace('.py', '')
 
-# run test suite using pytest
-elif args.test:
-	import pytest
-	from glob import glob
-	from sys import exit
-	test_paths = glob('./tests/*.py')
-	pytest_args = test_paths
+	if not ( args.scene or args.test or args.compile):
+		parser.error("scene is required unless --test or --compile are used")
+
+	# run test suite using pytest
+	elif args.test:
+		import pytest
+		from glob import glob
+		test_paths = glob('./tests/*.py')
+		pytest_args = test_paths
 
 
-	# run particular test
-	if type(args.test) is str and (
-		test_name := args.test.split('::')[-1] ).startswith('test'):
-		module_path = [ tp for tp in test_paths
-			if hasattr((mod := import_module( to_module(tp) )), test_name)
-		]
+		# run particular test
+		if type(args.test) is str and (
+			test_name := args.test.split('::')[-1] ).startswith('test'):
+			module_path = [ tp for tp in test_paths
+				if hasattr((mod := import_module( to_module(tp) )), test_name)
+			]
 
-		if not module_path:
-			print(f'could not find "{args.test}" test. candidates:')
-			from pprint import pp 
-			pp( list(f for m in module_path for f in m.__dict__ ))
-			exit(1)
+			if not module_path:
+				print(f'could not find "{args.test}" test. candidates:')
+				from pprint import pp 
+				pp( list(f for m in module_path for f in m.__dict__ ))
+				exit(1)
 
-		pytest_args = [ f'{module_path[0]}::{test_name}']
-	ret = pytest.main(['-qs', *pytest_args] )
-	exit(ret)
-		
+			pytest_args = [ f'{module_path[0]}::{test_name}']
+		ret = pytest.main(['-qs', *pytest_args] )
+		exit(ret)
+			
 
-# run or compile the scene
-else:
-	scene = args.scene
-	if args.compile:
-		from subprocess import run
-		#run( f'py -m mypyc -m common')
-		run( f'py -m mypyc -m ECS')
-		if scene: run( f'py -m nuitka --output-dir=build --standalone {scene}'.split(' ') )
+	# run or compile the scene
 	else:
-		import_module( to_module(scene) )
+		scene = args.scene
+		if args.compile:
+			from subprocess import run
+			run( f'py -m mypyc -m ECS', cwd=src.__path__[0])
+			if scene: run( f'py -m nuitka --output-dir=build --standalone {scene}'.split(' ') )
+		else:
+			import_module( to_module(scene) )
---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+
+import sys
+from pathlib import Path
+
+sys.path.append(f'{Path(__file__).parent}')
+
+#from .ECS import *
+#from .RenderContext import *
+#from .Utils import *
